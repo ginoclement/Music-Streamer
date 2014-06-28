81466b824c27be5a31232e97570d125384f5317c
Gino Clement <ginoclement@gmail.com>
2014-06-27 17:04:32 -0700

    Not really sure what I did, starting to work on the project again.

--- services/import.php
+++ services/import.php
@@ -9,4 +9,31 @@
 			Probably want to limit access to this file.
 	*/
 
+	//Create database object
+	// $db = new PDO("mysql:dbname=DATABASE;host=127.0.0.1", "USERNAME", "PASSWORD");
+	//Set error reporting
+	// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	//Processes a query
+	function query($query){
+		$q = $db->prepare("$query");
+		$q->execute();
+		return $q->fetchAll();
+	}
+
+	$xml = simplexml_load_file("Library.xml", "SimpleXMLElement", LIBXML_DTDLOAD);
+	// $temp = $xml->dict->dict->dict;
+	// print_r($xml);
+	$temp = $xml->dict->dict->dict;
+	// print_r($temp);
+
+	// foreach ($xml->key as $k) {
+	// 	print_r($k);
+	// }
+
+	foreach ($temp as $track) {
+		print_r($track->getChildren());
+	}
+
+
 ?>
